924fc0510a2fb6f259ba2f283904c3a341a442c9
Dasa122 <dasa@dasa122.net>
2026-09-10 13:13:01 +0200

    Add SQL script for querying student and teacher data

---
+++ 2/lekerdezesek.sql
@@ -0,0 +1,20 @@
+-- Active: 1789035636905@@127.0.0.1@3306
+USE Petrik;
+
+SELECT * FROM Diakok;
+
+SELECT nev, atlag
+FROM Diakok
+ORDER BY atlag DESC;
+
+SELECT nev, osztaly
+FROM Diakok
+WHERE atlag > 4.0;
+
+SELECT nev, tantargy
+FROM Tanarok
+WHERE tapasztalat > 10;
+
+SELECT terem_szam, kapacitas
+FROM Tantermek
+ORDER BY kapacitas DESC;
